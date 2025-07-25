6b8f22d5d27dbe06f01fd06752db18c52b521326
A7bert777 <2506245294@qq.com>
2025-07-25 22:02:28 +0000

    first commit

---
+++ build/CMakeFiles/rknn_resnet50_demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rknn_resnet50_demo.dir/src/main.cc.o"
+  "CMakeFiles/rknn_resnet50_demo.dir/src/resnet.cc.o"
+  "rknn_resnet50_demo"
+  "rknn_resnet50_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rknn_resnet50_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
